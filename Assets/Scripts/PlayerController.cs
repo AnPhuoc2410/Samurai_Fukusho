73ca0bfefda13e93a42ddfd5b4c4f7f8dc660855
Dao An Phuoc <anphuocdao2410@gmail.com>
2025-06-08 10:24:39 +0700

    feat: add player animations and controller updates

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,35 +4,120 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D _rigidbody;
-    private Vector2 _movementInput;
-    public float moveSpeed = 5f;
-    public bool IsMoving { get; private set; }
+    private Rigidbody2D rb;
+    public Animator animator;
+    private Vector2 moveInput;
 
+    public float walkSpeed = 5f;
+    public float runSpeed = 8f;
+
+    public float MoveSpeed
+    {
+        get
+        {
+            if (IsMoving)
+            {
+                if (IsRunning)
+                {
+                    return runSpeed;
+                }
+                else
+                {
+                    return walkSpeed;
+                }
+            }
+            else
+            {
+                //Idle
+                return 0f;
+            }
+        }
+    }
+
+    public bool _isFacingRight = true;
+
+    public bool IsFacingRight
+    {
+        get { return _isFacingRight; }
+        private set
+        {
+            if (_isFacingRight != value)
+            {
+                transform.localScale *= new Vector2(-1, 1);
+            }
+
+            _isFacingRight = value;
+        }
+    }
+
+    [SerializeField] private bool _isMoving = false;
+
+    public bool IsMoving
+    {
+        get { return _isMoving; }
+        private set
+        {
+            _isMoving = value;
+            animator.SetBool("isMoving", value);
+        }
+    }
+
+    [SerializeField] private bool _isRunnning = false;
+
+    public bool IsRunning
+    {
+        get { return _isRunnning; }
+        private set
+        {
+            _isRunnning = value;
+            animator.SetBool("isRunning", value);
+        }
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        _rigidbody = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        // Optional: Handle any non-physics-based logic here
-    }
-
-    // FixedUpdate is called at a fixed interval and is used for physics calculations
     void FixedUpdate()
     {
-        _rigidbody.linearVelocity = new Vector2(_movementInput.x * moveSpeed, _movementInput.y * moveSpeed);
+        rb.linearVelocity = new Vector2(moveInput.x * MoveSpeed, rb.linearVelocity.y);
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        _movementInput = context.ReadValue<Vector2>();
+        moveInput = context.ReadValue<Vector2>();
 
-        IsMoving = _movementInput != Vector2.zero;
+        IsMoving = moveInput != Vector2.zero;
+
+        SetFacingDirection(moveInput);
+    }
+
+    private void SetFacingDirection(Vector2 direction)
+    {
+        if (direction.x > 0 && !IsFacingRight)
+        {
+            IsFacingRight = true;
+        }
+        else if (direction.x < 0 && IsFacingRight)
+        {
+            IsFacingRight = false;
+        }
+    }
+
+
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            IsRunning = true;
+        }
+        else if (context.canceled)
+        {
+            IsRunning = false;
+        }
     }
 
     public void OnJump(InputAction.CallbackContext context)
