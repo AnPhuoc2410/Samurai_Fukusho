0a1758b9a239d071185b6dda9f9acc73e631980f
Anh140402 <anhnhmse181874@fpt.edu.vn>
2025-06-26 15:04:23 +0700

    update code for main menu

---
+++ Assets/Scripts/HowToPlayScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HowToPlayScript : MonoBehaviour
+{
+    public void HowToPlayGame()
+    {
+        SceneManager.LoadScene("HowToPlay"); 
+    }
+}
