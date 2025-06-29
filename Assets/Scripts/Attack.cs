bff65ae0232a8ed6e2bc09377790ab79b5a6d23d
Dao An Phuoc <anphuocdao2410@gmail.com>
2025-06-29 22:05:30 +0700

    feat: Enhance animations, add attack logic, optimize assets

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public int attackDamage = 10;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Damageable damageable = collision.GetComponent<Damageable>();
+        if (damageable != null)
+        {
+            damageable.Hit(attackDamage);
+            Debug.Log("Attacked " + collision.name + " for " + attackDamage + " damage.");
+        }
+    }
+}
+
